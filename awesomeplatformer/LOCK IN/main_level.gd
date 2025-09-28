c1163f93fdf0ba5d0355408946f42a95d322faf3
awadui <haqueawad@gmail.com>
2025-09-28 07:51:38 -0400

    canvaslayer setup

--- awesomeplatformer/LOCK IN/main_level.gd
+++ awesomeplatformer/LOCK IN/main_level.gd
@@ -1,6 +1,13 @@
 extends Node2D
 
-func _ready():
-	$Player.connect("health_changed", Callable($HUD, "update_health"))
-	$HUD.set_max_health($Player.max_health)
-	$HUD.update_health($Player.current_health)
+func _ready() -> void:
+	var player = $Player
+	var hud = $HUD   # HUD is the CanvasLayer
+
+	if not hud:
+		push_error("HUD node not found! Children: %s" % get_children())
+		return
+
+	hud.set_max_health(player.max_health)
+	hud.update_health(player.current_health)
+	player.health_changed.connect(Callable(hud, "update_health"))
