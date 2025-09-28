e8d1660581112818c11d07ae710ceb02d18de690
awadui <haqueawad@gmail.com>
2025-09-28 07:27:43 -0400

    quick changes to HUD

--- awesomeplatformer/LOCK IN/progress_bar.gd
+++ awesomeplatformer/LOCK IN/progress_bar.gd
@@ -1,6 +1,8 @@
 extends Control
-func set_max_health(value):
+
+func set_max_health(value: int):
 	$ProgressBar.max_value = value
 	
-func update_health(value):
+func update_health(value: int): 
 	$ProgressBar.value = value
+	$HealthText.text = "%d / %d" % [value, $HealthBar.max_value]
